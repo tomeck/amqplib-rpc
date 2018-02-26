69d5a12aa3aa3df772a8353b3b4560a8e0a8c75d
Tom Eck <teck@teck-iMac.local>
2018-02-25 20:42:45 -0500

    export doWorkflow

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   request: require('./lib/request.js'),
   reply: require('./lib/reply.js'),
+  doWorkflow: require('./lib/do-workflow.js'),
   checkReplyQueue: require('./lib/check-reply-queue.js'),
   checkQueue: require('./lib/check-queue.js'),
   TimeoutError: require('./lib/errors/timeout-error.js'),
